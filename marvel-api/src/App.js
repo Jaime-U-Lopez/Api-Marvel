3d05d935fb517d2552761f0e37bf778b31004222
Jaime Urrego <or4846@gmail.com>
2022-09-19 16:54:05 -0500

    ingreso de React Router Dom Funcionando

--- marvel-api/src/App.js
+++ marvel-api/src/App.js
@@ -1,9 +1,28 @@
 
 
+import {BrowserRouter  as  Router,Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Series from './pages/Series';
+import Comics from './pages/Comics';
+import Error404 from './pages/Error404';
 
 function App() {
   return (
-    <p>hola </p>
+    <div>    
+
+    <Router>
+  
+     <Routes>
+          <Route  path='/' exact component={()=><Home/>}/>
+          <Route path='/inicio' exact component={()=> <Home/>}/>
+          <Route path='/series' exact component={()=> <Series/>}/>
+          <Route path='/comics' exact component={()=> <Comics/>}/>
+          <Route component ={() =><Error404/>  }/>
+       </Routes>
+
+      </Router>
+    </div>
+
   );
 }
 
