2dcaa6f2538a1591902948e74b96f2b05b0732eb
Jaime Urrego <or4846@gmail.com>
2022-09-22 16:45:07 -0500

    llamado a api y estilos listos

--- marvel-api/src/components/LastEvent/LastEvents.js
+++ marvel-api/src/components/LastEvent/LastEvents.js
@@ -1,13 +1,58 @@
 
+import { Card,Icon,Image,Dimmer,Loader,Button } from 'semantic-ui-react'
 import './LastEvent.scss'
 
-export default function LastEvents(params) {
+export default function LastEvents({LastEventsFetch}) {
+const {loading,result}= LastEventsFetch;
+
+if(loading || !result){
+    return (
+   <Dimmer active inverted>
+   <Loader  inverted>Loading...</Loader>
+   </Dimmer>
+   )
+}
+
+
+
+const {results}=result.data;
+console.log(results) 
 
     return(
+    
+        results.map((event,index)=>(  
+            <Card key={index} className ="last-event">
+                <Image src={`${event.thumbnail.path}.${event.thumbnail.extension}`}  wrapped ui={false}/>
+                <Card.Content>
+                    <Card.Header className='titleEvent'  >{event.title} </Card.Header>
+                    <Card.Meta> 
+                        <span>
+                            <Icon name="book"/>
+                      
+                            {event.characters.available} Comics
 
-        <div>
-            
-        </div>
+                        </span>
+                    </Card.Meta>
+
+                    <Card.Description> {event.description }</Card.Description>
+
+                </Card.Content>
+
+                <Card.Content extra>
+                    <Button animated fluid as="a" href={event.urls[0].url} target="_blank" color='black'>
+
+                    <Button.Content visible> ver Evento</Button.Content>
+                    <Button.Content hidden> 
+                    <Icon name="arrow right"/>
+
+                    </Button.Content>
+                    </Button>
+                </Card.Content>
+            </Card>
+
+         ) )
+
+        
 
     )
     
--- marvel-api/src/components/ListLastEvents/ListLastEvents.js
+++ marvel-api/src/components/ListLastEvents/ListLastEvents.js
@@ -3,10 +3,11 @@ import { Card,Header } from 'semantic-ui-react';
 import Containers from '../Containers';
 import useFetch from '../Hooks/UseFetch';
 import './ListLastEvents.scss';
+import LastEvents from '../LastEvent/LastEvents';
 
 export default function ListLastEvents(params) {
     
- 
+ //realizar el llamado a la informacion de la api de marvel
     const LastEventsFetch=useFetch(    
         `${process.env.REACT_APP_API_URL_BASE2}/comics?ts=1&apikey=${process.env.REACT_APP_API_KEY_MARVEL}&hash=${process.env.REACT_APP_HASH_ID}&orderBy=title&limit=5`,
        //'http://gateway.marvel.com/v1/public/comics?ts=1&apikey=b4dd4282e1b25f7c72aa3cba77ab4ce4&hash=4232dd5bcab38413ba6d13fd93d1ec65&orderBy=title&limit=5'  
@@ -21,14 +22,8 @@ export default function ListLastEvents(params) {
                </Header>
             <Containers  bg="light">
                 <Card.Group itemsPerRow={5}>
+                    <LastEvents  LastEventsFetch={LastEventsFetch} />
 
-                    <p>los eventos </p>
-                    <p>los eventos </p>
-                    <p>los eventos </p>
-                    <p>los eventos </p>
-                    <p>los eventos </p>
-                    <p>los eventos </p>
-                    <p>los eventos </p>
                 </Card.Group>
 
             </Containers>
